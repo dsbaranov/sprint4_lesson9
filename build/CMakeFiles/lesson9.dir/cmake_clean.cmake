77339ecaee63bf5e1e993c6d12898367c360e4ee
dsbaranov <DmitryTheRedHead@yandex.ru>
2024-06-27 00:15:12 +0300

    added QueryResult deque manager

---
+++ build/CMakeFiles/lesson9.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson9.dir/main.cpp.o"
+  "CMakeFiles/lesson9.dir/main.cpp.o.d"
+  "lesson9"
+  "lesson9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lesson9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
